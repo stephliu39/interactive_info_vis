a201842020883ba14d24f3bbf61ece776d0e3fe5
stephanie <114697344+stephliu39@users.noreply.github.com>
2025-10-25 16:32:48 -0700

    Converted time to float and added blooming/wilting cycles

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -4,11 +4,20 @@ registerSketch('sk2', function (p) {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
   p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+     background(230, 245, 255);
+    translate(width / 2, height / 2);
+
+    let h = hour();
+    let m = minute();
+    let s = second();
+
+    // Convert current time to 24-hour float
+    let time24 = h + m / 60 + s / 3600;
+
+    // Blooming/wilting cycle (0–1 for 12am→12pm, 1–0 for 12pm→12am)
+    let bloomProgress = time24 < 12
+      ? map(time24, 0, 12, 0, 1)
+      : map(time24, 12, 24, 1, 0);
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
