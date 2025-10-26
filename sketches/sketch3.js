f4429e543e8572d5bea45f07336035200db307fb
stephanie <114697344+stephliu39@users.noreply.github.com>
2025-10-25 21:04:19 -0700

    Implemented set up for second clock

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -2,6 +2,18 @@
 registerSketch('sk3', function (p) {
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textSize(16);
+    p.noStroke();
+
+    baseY = p.height / 2 + 100;
+
+    input = p.createInput('60');
+    input.position(20, p.height + 20);
+
+    button = p.createButton('Start Timer');
+    button.position(input.x + input.width + 10, p.height + 20);
+    button.mousePressed(startTimer);
   };
   p.draw = function () {
     p.background(240, 200, 200);
