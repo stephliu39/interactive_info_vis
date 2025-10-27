7aa1d2b18459b51c5864db863d4cca616cbad720
stephanie <114697344+stephliu39@users.noreply.github.com>
2025-10-27 16:28:43 -0700

    Added intial set up

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,15 +1,28 @@
 // Instance-mode sketch for tab 4
 registerSketch('sk4', function (p) {
-  
+  let iceCubes = [];
+  let running = false;
+  let inBreak = false;
+  let startTime = null;
+  let timeRemaining = 0;
+  let totalDuration = 0;
+  let breakTimeRemaining = 0;
+  let breakTotalDuration = 5 * 60 * 1000;
+  let startButton, pauseButton, resetButton, skipButton;
+  let focusInput, breakInput;
+
 
   p.setup = function () {
      p.createCanvas(p.windowWidth, p.windowHeight);
+     p.textFont("Inter, Helvetica, Arial, sans-serif");
   };
 
    p.draw = function () {
 
    }
 
- 
+   // Functions for timer UI
+   
+
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
